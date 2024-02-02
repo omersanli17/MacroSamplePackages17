678788925e48492c0c3e88e1375a31e5b777c8e2
Omer <sanli.omer@metu.edu.tr>
2024-02-02 22:19:42 +0300

    Add: Mark notation

--- Tests/MacroSamplePackages17Tests/MacroSamplePackages17Tests.swift
+++ Tests/MacroSamplePackages17Tests/MacroSamplePackages17Tests.swift
@@ -3,6 +3,7 @@ import SwiftSyntaxMacrosTestSupport
 import XCTest
 
 // Macro implementations build for the host, so the corresponding module is not available when cross-compiling. Cross-compiled tests may still make use of the macro itself in end-to-end tests.
+
 #if canImport(MacroSamplePackages17Macros)
 import MacroSamplePackages17Macros
 
@@ -129,6 +130,8 @@ final class MacroSamplePackages17Tests: XCTestCase {
         )
     }
     
+    // MARK: ATTACHED MEMBER MACRO TEST
+    
     func testDictionaryStorageMemberInitilizationPurposes() {
         assertMacroExpansion(
             """
@@ -152,7 +155,9 @@ final class MacroSamplePackages17Tests: XCTestCase {
         )
     }
     
-    func testDictionaryStoragePropertyMemberAttribute() {
+    // MARK: ATTACHED MEMBER_ATTRIBUTE MACRO TEST
+    
+    func testMemberAttribute() {
         assertMacroExpansion(
             """
             @Observable
@@ -174,6 +179,7 @@ final class MacroSamplePackages17Tests: XCTestCase {
         )
     }
     
+    // MARK: ATTACHED EXTENSION  MACRO TEST
     func testExtensionMacro() {
         assertMacroExpansion(
                 """
